9b5d0570ebe3702bdf5117ef5e3e8b41ac300e76
Farhan Dwi Anugrah <61016542+Farhan-19@users.noreply.github.com>
2021-01-07 21:16:35 +0700

    Add files via upload

---
+++ deret.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int barisderet, n, x, y;
+
+    cout << "Baris Deret Yang Muncul Yaitu :";
+    cin>>barisderet; 
+	y=7;
+    for(x=1; x<barisderet; x++)
+    {
+    cout <<y<<"";
+	y=y+7;
+
+    };
+    return 0;
+}
